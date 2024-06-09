7fd7e07b5c0dcbc57c185539240baeb22b7ed9f6
chanil <hci970705@gmail.com>
2024-06-09 20:39:23 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Header</h1>
+        <ul>
+            <router-link :to="{ name: 'Home' }"></router-link>
+        </ul>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
