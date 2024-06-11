5d12e892e9bfc51fc276c5b40d7c61947bf487ad
chanil <hci970705@gmail.com>
2024-06-11 12:32:42 +0900

    IEButton 기능 구현

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
         <h1>Header</h1>
+        <IEButton />
         <ul>
             <router-link :to="{ name: 'HomeExpense' }"></router-link>
         </ul>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import IEButton from './IEButton.vue';
+</script>
 
 <style lang="scss" scoped></style>
--- src/components/IEButton.vue
+++ src/components/IEButton.vue
@@ -1,7 +1,22 @@
 <template>
-    <div></div>
+    <div class="container">
+        <button class="btn btn-primary" @click="gotoExpense">지 출</button>
+        &nbsp;&nbsp;<button class="btn btn-primary" @click="gotoIncome">
+            수 입
+        </button>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const currentRoute = useRouter();
+const gotoExpense = () => {
+    currentRoute.push('/Expense');
+};
+const gotoIncome = () => {
+    currentRoute.push('/Income');
+};
+</script>
 
 <style lang="scss" scoped></style>
