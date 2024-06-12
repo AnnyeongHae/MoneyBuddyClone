b18bbfb142debd5a7ad52201b8a135bee35a373e
zhyeon <pjh101124@gmail.com>
2024-06-12 11:24:29 +0900

    커밋 2

--- src/pages/Input.vue
+++ src/pages/Input.vue
@@ -1,12 +1,24 @@
 <template>
   <form>
-    <div>
-      <h2>지출</h2>
-    </div>
-
     <div class="a">
       <label for="date">날짜&nbsp;</label>
       <input type="date" v-model="date" id="date" placeholder="Placeholder" />
+    </div>
+
+    <div class="app">
+      <label for="category">카테고리&nbsp;</label>
+
+      <select v-model="selectedCategory">
+        <option value="">카테고리를 선택하세요</option>
+
+        <option
+          v-for="category in categories"
+          :value="category.id"
+          :key="category.id"
+        >
+          {{ category.name }}
+        </option>
+      </select>
     </div>
 
     <div class="fix-button">
@@ -39,12 +51,34 @@
       <textarea v-model="memo" id="memo" placeholder="Placeholder"></textarea>
     </div>
 
-    <div id="app">
+    <div id="f">
       <button type="reset" @click="reset">다시 쓰기</button>
       <button type="submit" @click="submit">완료</button>
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      selectedCategory: '',
+      categories: [
+        { id: 1, name: '식비' },
+        { id: 2, name: '생활용품' },
+        { id: 3, name: '패션미용' },
+        { id: 4, name: '문화생활' },
+        { id: 5, name: '교육' },
+        { id: 6, name: '의료비' },
+        { id: 7, name: '교통비' },
+        { id: 8, name: '공과금' },
+        { id: 9, name: '기타' },
+      ],
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 * {
