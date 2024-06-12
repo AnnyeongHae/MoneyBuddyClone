b4845a2b97bff8f56633dccbd5413af318415f86
KIM EUN HYE <keh94330@naver.com>
2024-06-12 16:38:27 +0900

    BudgetItem추가

--- src/components/BudgetItem.vue
+++ src/components/BudgetItem.vue
@@ -1,9 +1,98 @@
 <template>
-    <div>
-        <h2>Daily</h2>
+  <div class="form-group">
+    <label for="date">날짜</label>
+    <input type="date" v-model="date" id="date" placeholder="Placeholder" />
+  </div>
+
+  <div class="entry">
+    <div class="entry-header">
+      <span>금액</span>
+      <div class="entry-actions">
+        <button class="edit-button">편집</button>
+        <button class="delete-button">삭제</button>
+      </div>
     </div>
+
+    <div class="entry-body">
+      <span>카테고리</span>
+    </div>
+    &nbsp;&nbsp;&nbsp;
+  </div>
+  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+  <div class="entry">
+    <div class="entry-header">
+      <span>금액</span>
+      <div class="entry-actions">
+        <button class="edit-button">편집</button>
+        <button class="delete-button">삭제</button>
+      </div>
+    </div>
+
+    <div class="entry-body">
+      <span>카테고리</span>
+    </div>
+    &nbsp;&nbsp;&nbsp;
+  </div>
+  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+  <div class="entry">
+    <div class="entry-header">
+      <span>금액</span>
+      <div class="entry-actions">
+        <button class="edit-button">편집</button>
+        <button
+          class="delete-button"
+          key:class="btn btn primary m-1"
+          @click="router.push('/Daily')"
+        >
+          삭제
+        </button>
+      </div>
+    </div>
+
+    <div class="entry-body">
+      <span>카테고리</span>
+    </div>
+    &nbsp;&nbsp;&nbsp;
+  </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  name: 'Daily',
+};
+</script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.date-label {
+  margin-bottom: 5px;
+}
+
+.date-input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.entry {
+  background-color: #e0e0e0;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+.entry-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #000;
+  padding-bottom: 5px;
+}
+
+.entry-actions button {
+  margin-left: 5px;
+}
+
+.entry-body {
+  padding-top: 5px;
+}
+</style>
