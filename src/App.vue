6922fbff8df94529d551babf65b3b8dd484eebae
chanil <hci970705@gmail.com>
2024-06-12 14:53:38 +0900

    useBudgetListStore 구현 + App.vue fetch 가능 확인

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,18 @@
 
 <script setup>
 import Header from '@/components/Header.vue';
-// import { computed } from 'vue';
+import { computed } from 'vue';
 import { useBudgetListStore } from './stores/budget';
+import { onMounted } from 'vue';
 
 const budgetListStore = useBudgetListStore();
 const fetchPeriodic = budgetListStore.fetchPeriodic;
-// const fetchBudget = budgetListStore.fetchBudget;
+const fetchBudget = budgetListStore.fetchBudget;
 
-fetchPeriodic();
-// fetchBudget();
+onMounted(() => {
+    fetchPeriodic();
+    fetchBudget();
+});
 </script>
 
 <style lang="scss" scoped></style>
